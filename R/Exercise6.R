3849311d2bb398965cefd5b8bdac86f02885eec0
Lu Qiaomu <Lu.Qiaomu@campus.lmu.de>
2024-07-08 04:02:31 +0200

    Add examples in every function comments

--- R/Exercise6.R
+++ R/Exercise6.R
@@ -3,9 +3,11 @@
 #' @param f_temp The Fahrenheit temperature to be converted
 #'
 #' @return The converted Celsius temperature
-#' @export
 #'
-#' @examples
+#' @examples F_to_C(100)
+#' @examples F_to_C(0)
+#'
+#' @export
 F_to_C <- function(f_temp){
   c_temp <- (f_temp - 32) * 5/9;
   return(c_temp);
@@ -16,9 +18,11 @@ F_to_C <- function(f_temp){
 #' @param c_temp The Celsius temperature to be converted
 #'
 #' @return The converted Fahrenheit temperature
-#' @export
 #'
-#' @examples
+#' @examples C_to_F(100)
+#' @examples C_to_F(0)
+#'
+#' @export
 C_to_F <- function(c_temp){
   f_temp <- (c_temp * 9/5) + 32;
   return(f_temp);
@@ -29,9 +33,11 @@ C_to_F <- function(c_temp){
 #' @param k_temp The Kelvin temperature to be converted
 #'
 #' @return The converted Celsius temperature
-#' @export
 #'
-#' @examples
+#' @examples K_to_C(100)
+#' @examples K_to_C(0)
+#'
+#' @export
 K_to_C <- function(k_temp){
   c_temp <- k_temp - 273.15;
   return(c_temp);
@@ -42,9 +48,11 @@ K_to_C <- function(k_temp){
 #' @param c_temp The Celsius temperature to be converted
 #'
 #' @return The converted Kelvin temperature
-#' @export
+##'
+#' @examples C_to_K(100)
+#' @examples C_to_K(0)
 #'
-#' @examples
+#' @export
 C_to_K <- function(c_temp){
   k_temp <- c_temp + 273.15;
   return(k_temp);
@@ -55,9 +63,11 @@ C_to_K <- function(c_temp){
 #' @param k_temp The Kelvin temperature to be converted
 #'
 #' @return The converted Fahrenheit temperature
-#' @export
 #'
-#' @examples
+#' @examples K_to_F(100)
+#' @examples K_to_F(0)
+#'
+#' @export
 K_to_F <- function(k_temp){
   f_temp <- ((k_temp - 273.15) * 9/5) + 32;
   return(f_temp);
@@ -68,9 +78,11 @@ K_to_F <- function(k_temp){
 #' @param f_temp The Fahrenheit temperature to be converted
 #'
 #' @return The converted Kelvin temperature
-#' @export
 #'
-#' @examples
+#' @examples F_to_K(100)
+#' @examples F_to_K(0)
+#'
+#' @export
 F_to_K <- function(f_temp){
   k_temp <- (f_temp - 32) * 5/9 + 273.15;
   return(k_temp);
